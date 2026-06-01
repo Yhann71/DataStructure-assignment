68630c32bb935549e0b42e77d1998036a5d6c78f
Yhann71 <tan28174@gmail.com>
2026-06-01 17:26:57 +0800

    Update RobotAssignment.hpp

--- DSTR v1.1/RobotAssignment.hpp
+++ DSTR v1.1/RobotAssignment.hpp
@@ -2,20 +2,18 @@
 #define ROBOT_ASSIGNMENT_HPP
 
 #include <string>
-#include "OrderManagement.hpp" // Required to access the Order data structure
+#include "OrderManagement.hpp"
 
 using namespace std;
 
-// Maximum size constraint for the custom Robot array queue
 #define MAX_ROBOTS 5
 
 struct Robot {
     string robotID;
-    string status;       // Track states: "Available", "Busy", "Maintenance"
-    int tasksCompleted;  // tracks individual work count to show fair distribution
+    string status;       // "Available", "Busy", "Maintenance"
+    int tasksCompleted;  // Tracks individual work distribution
 };
 
-// Hand-built Circular Queue matching Task requirements
 struct RobotQueue {
     Robot arr[MAX_ROBOTS];
     int front;
